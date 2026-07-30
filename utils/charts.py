7fc457b51d60b1c3e9857bc06e50da661a536349
Calmigodetem <r.valenta.ch@gmail.com>
2026-07-30 13:55:04 +0200

    Update charts.py

--- utils/charts.py
+++ utils/charts.py
@@ -2,19 +2,37 @@ import plotly.express as px
 
 
 def balance_chart(df):
-
     fig = px.line(
-        df,
+        df.sort_values("datum zaúčtování"),
         x="datum zaúčtování",
         y="zůstatek",
-        markers=True,
         title="Vývoj zůstatku"
     )
 
     fig.update_layout(
-        xaxis_title="Datum",
-        yaxis_title="Kč",
-        height=450
+        height=400,
+        margin=dict(l=20, r=20, t=50, b=20)
+    )
+
+    return fig
+
+
+def category_chart(df):
+
+    expenses = df[df["částka platby"] < 0].copy()
+
+    summary = (
+        expenses.groupby("Kategorie")["částka platby"]
+        .sum()
+        .abs()
+        .reset_index()
+    )
+
+    fig = px.pie(
+        summary,
+        names="Kategorie",
+        values="částka platby",
+        title="Výdaje podle kategorií"
     )
 
     return fig
